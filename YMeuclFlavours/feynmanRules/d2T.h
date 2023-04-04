0f7015fa905b048669237f07b71d23e2aeaf690f
Nikolai Husung <n.husung@soton.ac.uk>
2023-04-04 15:36:38 +0100

    Adjusted (some of the) simplifyRules and dropped not needed Feynman rules.

--- YMeuclFlavours/feynmanRules/d2T.h
+++
@@ -1,9 +0,0 @@
-
-#ifdef `singlet'
-#define Q "quark";
-#else
-#define Q "quark2";
-#endif
-
-id vert(aquark(p?,alpha?,cf1?,fl1?),`Q'(q?,beta?,cf2?,fl2?),src(?args)) =
-   +(p.p+2*p.q+q.q)*(FL(fl1,kappahat,lambdahat,fl2)-FL(fl1,lambdahat,kappahat,fl2))*d_(cf1,cf2)*OPF(aquark,`Q',fl1)/2;
